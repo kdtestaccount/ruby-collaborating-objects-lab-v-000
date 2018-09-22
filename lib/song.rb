578b31d91a137f4d8fd0c1230da071bc875b41ca
kvdoherty <kvdoherty@gmail.com>
2018-09-22 14:47:49 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,7 +23,7 @@ def self.new_by_filename(file_name)
   
   newsong = Song.new(@name, @artist)
   newsong.artist.name = @artist
-  newsong.artist.all << @artist
+  newsong.all << @artist
   artistreturned = newsong.artist(newsong)
   newsong
 end 
