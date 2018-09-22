4d4c54088f6c591dcb831110826f10eabbfcfad4
kvdoherty <kvdoherty@gmail.com>
2018-09-22 15:09:01 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,8 +31,9 @@ end
 def artist(artist_name=nil)
   @artist_inst = Artist.find_or_create_by_name(artist_name)
   @artist_inst.add_song(artist_name)
+  
   Artist.all << @artist_inst
-  puts Artist.all
+  
   @artist_inst
 end
 
